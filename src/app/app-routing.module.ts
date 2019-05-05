ce9c0378cd6b0f9b12f0cae622b92b35745849ef
JAIME BRAVO GALAN <jbravoga@everis.com>
2019-05-05 16:14:34 +0200

    improvement styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { VHomeComponent } from './views/v-home/v-home.component';
-import { CDetailsComponent } from './components/c-details/c-details.component';
 
 const routes: Routes = [
   {
